e1101efee81b72e1286483b2a417dfdb49948b45
Shawoor Ayyoob <mohdshawoor@gmail.com>
2025-01-30 10:37:04 +0530

    New Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 import Message from './Components/Message';
 import Cars from './Components/Cars';
 import CourseList from './Components/CourseList';
+import Products from './Components/Products';
 function App() {
   return (
     <div>
-      <h1>Courses</h1>
-      <CourseList />
+      <Products />
     </div>
   );
 }
