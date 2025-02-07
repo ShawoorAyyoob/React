ae07ee7608c5b7842fff696ac71db748aeaaab1e
ShawoorAyyoob <mohdshawoor@gmail.com>
2025-02-07 12:07:45 +0530

    Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -22,16 +22,48 @@ import ProductList from "./Components/ProductList";
 import RecipeList from "./Components/RecipeList";
 import Home from "./Components/Home";
 import AboutUs from "./Components/AboutUs";
+import Message from "./Components/Message";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About Us</Link>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      {/* <Link to="/">React App</Link> | <Link to="/">Home</Link> | <Link to="/about">About Us</Link> | <Link to="/component">Components</Link> */}
+        <div className="container-fluid">
+    <a className="navbar-brand" href="#"><Link to="/">React App</Link></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#"><Link to="/">Home</Link></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#" ><Link to="/about">About Us</Link></a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link to="/component">Components</Link>
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Recipes</a></li>
+            <li><a className="dropdown-item" href="#">Toggle</a></li>
+            <li><a className="dropdown-item" href="#">Message</a></li>
+            <li><a className="dropdown-item" href="#">User List</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="/component" element={<RecipeList/>}></Route>
+        <Route path="/component" element={<Toggle/>}></Route>
+        <Route path="/component" element={<Message/>}></Route>
+        <Route path="/component" element={<UserList/>}></Route>
       </Routes>
     </Router>
   );
